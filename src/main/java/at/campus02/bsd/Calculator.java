7eb2a79574ca36ca559d8f57117b4bcde88821d0
Lukas Grafoner <s50706@edu.campus02.at>
2022-05-18 11:33:25 +0200

    added logs to gitignore, edited pom and Calculator class

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -3,25 +3,31 @@ package at.campus02.bsd;
 public class Calculator {
 
     public double add(double number1, double number2){
+        Main.logger.debug(number1 + " + "+ number2);
         return number1 + number2;
     }
 
     public double minus(double number1, double number2){
+        Main.logger.debug(number1 + " - "+ number2);
         return number1 - number2;
     }
 
     public double divide(double number1, double number2) throws ArithmeticException{
+        Main.logger.debug(number1 + " / "+ number2);
         if(number2 == 0){
+            Main.logger.error(number1 + " / "+ number2 + " --> Division by zero!");
             throw new ArithmeticException("Division by zero.");
         }
         return number1 / number2;
     }
 
     public double multiply(double number1, double number2){
+        Main.logger.debug(number1 + " * "+ number2);
         return number1 * number2;
     }
 
     public int calcFaculty(int n){
+        Main.logger.debug(n +"!");
         if(n <= 0){
             return 0;
         }
