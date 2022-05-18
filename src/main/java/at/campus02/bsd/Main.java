a4dc5d98f8af981e094c50bec5e4b4cb5116354c
Lukas Grafoner <s50706@edu.campus02.at>
2022-05-18 10:42:11 +0200

    Added Logger to Main

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,14 +1,24 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    public static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
         try{
             Calculator calc = new Calculator();
 
             System.out.println("1 + 2 = "+calc.add(1, 2));
+            logger.info("Dies ist ein Info-Log!");
+
             System.out.println("45 - 5 = "+calc.minus(45, 5));
+
             System.out.println("9 * 4 = "+calc.multiply(9, 4));
+            logger.error("Dies ist ein Error-Log!");
+
             System.out.println("15 / 2 = "+calc.divide(15, 2));
 
         }catch(Exception e){
